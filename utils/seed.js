99ce0e990a5fc396a0aca0949edc674027cbe05b
Barry25000 <barryengler@gmail.com>
2023-07-13 22:33:46 -0500

    seed data

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,6 @@
-const { User, Thought, Reaction } = require("../models");
-const mongoose = require("mongoose");
+// const { User, Thought, Reaction } = require("../models");
+const { User } = require("../models");
+// const mongoose = require("mongoose");
 
 const connection = require("../config/connection");
 
@@ -10,21 +11,55 @@ const users = [
     email: "barry@gmail.com",
     thought: [],
   },
+  {
+    username: "Ben Ten",
+    email: "ben@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Big Red",
+    email: "b.red@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Smokey_and_the_bandit",
+    email: "did.i.see.a.bear@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Bugs.Bunny",
+    email: "bugs@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Yetti",
+    email: "hide.n.seek.champ@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Nessy",
+    email: "do_you_see_me@gmail.com",
+    thought: [],
+  },
+  {
+    username: "Al.Copone",
+    email: "tommy_gun@gmail.com",
+    thought: [],
+  },
 ];
-
-console.log(connection);
 
 // Connects to server
 connection.once("open", async () => {
   console.log("connected");
 
-  // Delete existing students
+  // Drop existing users
   await User.deleteMany({});
 
-  // Adds seed data to database
+  // Add users to the collection and await the results
   await User.collection.insertMany(users);
 
+  // Log out the seed data to indicate what should appear in the database
   console.table(users);
-  console.info("Seeding complete!");
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
